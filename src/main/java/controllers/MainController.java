30c711679362e910a3390334a1b055bab92cfd5b
Ruaridh <ruaridhdunbar@gmail.com>
2018-05-25 16:15:19 +0100

    adds main controller

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,4 +1,37 @@
 package controllers;
 
+import db.Seeds;
+import models.Article;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
 public class MainController {
+
+    public static void main(String[] args) {
+
+        Seeds.seedData();
+
+        staticFileLocation("/public");
+
+        LoginController loginController = new LoginController();
+        JournalistController journalistController = new JournalistController();
+        ArticleController articleController = new ArticleController();
+
+        get("/", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            String loggedInUser = LoginController.getLoggedInUserName(req, res);
+            model.put("user", loggedInUser);
+            model.put("template", "templates/main.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+
+    }
 }
