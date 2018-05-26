575a0ec547de1421ca81b1290e3e227cafea3be5
Ruaridh <ruaridhdunbar@gmail.com>
2018-05-26 11:29:06 +0100

    fixes search methods, tests to check output

--- src/main/java/controllers/JournalistController.java
+++ src/main/java/controllers/JournalistController.java
@@ -70,7 +70,7 @@ public class JournalistController {
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
-        post ("/journalists", (req, res) -> {
+        post ("/admin/journalists", (req, res) -> {
             String name = req.queryParams("name");
             Journalist journalist = new Journalist(name);
             DBHelper.save(journalist);
@@ -78,7 +78,7 @@ public class JournalistController {
             return null;
         }, new VelocityTemplateEngine());
 
-        post ("/journalists/:id/delete", (req, res) -> {
+        post ("/admin/journalists/:id/delete", (req, res) -> {
             int id = Integer.parseInt(req.params(":id"));
             Journalist journalistToDelete = DBHelper.find(id, Journalist.class);
             DBHelper.delete(journalistToDelete);
@@ -86,7 +86,7 @@ public class JournalistController {
             return null;
         }, new VelocityTemplateEngine());
 
-        post ("/journalists/:id", (req, res) -> {
+        post ("/admin/journalists/:id", (req, res) -> {
             String strId = req.params(":id");
             Integer intId = Integer.parseInt(strId);
             Journalist journalist = DBHelper.find(intId, Journalist.class);
